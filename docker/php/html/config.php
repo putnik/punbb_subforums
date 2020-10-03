1e2104b0e7d10bb6803134cdd79dc0773f9900a8
Sergey Leschina <mail@putnik.tech>
2020-10-03 09:42:15 +0300

    docker env for development

---
+++ docker/php/html/config.php
@@ -0,0 +1,42 @@
+<?php
+
+$db_type = 'mysqli_innodb';
+$db_host = '172.19.0.2';
+$db_name = 'punbb';
+$db_username = 'punbb';
+$db_password = 'secret';
+$db_prefix = 'punbb_';
+$p_connect = false;
+
+$base_url = 'http://punbb.local';
+
+$cookie_name = 'forum_cookie_635e02';
+$cookie_domain = '';
+$cookie_path = '/';
+$cookie_secure = 0;
+
+define('FORUM', 1);
+
+// Enable DEBUG mode by removing // from the following line
+define('FORUM_DEBUG', 1);
+
+// Enable show DB Queries mode by removing // from the following line
+define('FORUM_SHOW_QUERIES', 1);
+
+// Enable forum IDNA support by removing // from the following line
+//define('FORUM_ENABLE_IDNA', 1);
+
+// Disable forum CSRF checking by removing // from the following line
+//define('FORUM_DISABLE_CSRF_CONFIRM', 1);
+
+// Disable forum hooks (extensions) by removing // from the following line
+//define('FORUM_DISABLE_HOOKS', 1);
+
+// Disable forum output buffering by removing // from the following line
+//define('FORUM_DISABLE_BUFFERING', 1);
+
+// Disable forum async JS loader by removing // from the following line
+//define('FORUM_DISABLE_ASYNC_JS_LOADER', 1);
+
+// Disable forum extensions version check by removing // from the following line
+//define('FORUM_DISABLE_EXTENSIONS_VERSION_CHECK', 1);
